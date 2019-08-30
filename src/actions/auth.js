cb5c48a34306128c31d46562c598ca2cfe17b9ba
Duong Lam <dzuongld@gmail.com>
2019-08-30 19:08:31 +1000

    Signin with redirect instead of popup

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,23 +1,23 @@
-import { auth, googleAuthProvider } from '../firebase/firebase';
+import { auth, googleAuthProvider } from '../firebase/firebase'
 
 export const login = (uid) => ({
     type: 'LOGIN',
     uid
-});
+})
 
 export const startLogin = () => {
     // console.log('login click');
     return () => {
-        return auth.signInWithPopup(googleAuthProvider);
+        return auth.signInWithRedirect(googleAuthProvider)
     }
 }
 
 export const logout = () => ({
     type: 'LOGOUT'
-});
+})
 
 export const startLogout = () => {
     return () => {
-        return auth.signOut();
+        return auth.signOut()
     }
 }
