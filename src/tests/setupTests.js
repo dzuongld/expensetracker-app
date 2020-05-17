4ae16b291189dcc8f500965e260bb4a72d85eb0d
Duong Lam <dzuongld@gmail.com>
2020-05-17 11:13:21 +1000

    Testing updates

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,9 +1,9 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import DotEnv from 'dotenv';
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import DotEnv from 'dotenv'
 
-Enzyme.configure({
+configure({
     adapter: new Adapter()
-});
+})
 
-DotEnv.config({path: '.env.test'});
+DotEnv.config({ path: '.env.test' })
